de7ef2a006c4ff626fc2310272d450f4ac268117
xinshaoguang <997888255@qq.com>
2020-12-01 15:12:58 +0800

    引入lodash，页面中使用

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  mounted() {
+    console.dir(_, "_");
+    var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 23];
+    var a = _.chunk(arr, 3);
+    console.log(a, "a");
+    // s
+    var obj = {
+      name: "liudehua",
+      age: {
+        age: 58,
+      },
+    };
+    var obj1 = _.cloneDeep(obj);
+    obj.age.age = 60;
+    console.log(obj, obj1);
+  },
+};
 </script>
